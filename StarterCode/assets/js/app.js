b5f68a4aa4a69e11bba3bcf1532b562b40509f03
Felix Pronove <felixpronove4@gmail.com>
2021-04-07 13:41:48 -0600

    Starting to develop area for graphic

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,27 @@
 // @TODO: YOUR CODE HERE!
+// You need to create a scatter plot between two of the data variables such as `Healthcare vs. Poverty` or `Smokers vs. Age`
+
+// Chart Params
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = { top: 20, right: 40, bottom: 60, left: 50 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import data from the data csv file
+d3.csv("../data/data.csv").then(function(StateData) {
+    console.log(StateData);
+    console.log([StateData]);
+});
