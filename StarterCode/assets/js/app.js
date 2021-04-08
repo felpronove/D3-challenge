7b500c05bc0f1939f675039ce6bade2890624293
Felix Pronove <felixpronove4@gmail.com>
2021-04-08 16:52:33 -0600

    Added more information to the javascript file

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -19,9 +19,36 @@ var svg = d3
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
+  
 // Import data from the data csv file
-d3.csv("../data/data.csv").then(function(StateData) {
+d3.csv("assets/data/data.csv").then(function(StateData) {
     console.log(StateData);
     console.log([StateData]);
+
+    // sort through the data
+    StateData.forEach(function(data) {
+
+    })
+    
+    // create scales
+    // compare income levels vs obesity levels
+    var xLinearScale = d3.scaleLinear()
+        .domain(d3.extent(StateData, d => d.income))
+        .range([0, width]);
+  
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(StateData, d => d.obesity)])
+        .range([height, 0]);
+
+    // create axes
+    var xAxis = d3.axisBottom(xLinearScale).ticks(10);
+    var yAxis = d3.axisLeft(yLinearScale).ticks(10);
+
+    // append axes
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    chartGroup.append("g")
+      .call(yAxis);
 });
